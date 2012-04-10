9e7df279351f833ef3f47ba7a906bf0b37184637
08404235783 <08404235783@serpro-1524623.fns.serpro>
2012-04-10 18:33:04 -0300

    Ajustes visuais.

--- src/java/com/jota/infopesca/bean/Peixe.java
+++ src/java/com/jota/infopesca/bean/Peixe.java
@@ -4,17 +4,10 @@
  */
 package com.jota.infopesca.bean;
 
+import com.jota.infopesca.annotations.GridConfig;
 import java.io.Serializable;
 import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
